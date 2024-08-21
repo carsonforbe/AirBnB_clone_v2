c3568f4f120cd8f0ddc2d6ef8919cc447c026183
HackerSkull <hackerskull@HackerSkull.hackerskull>
2024-08-21 06:16:09 -0400

    comments added on file 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+Starts a flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
+
 @app.route('/', strict_slashes=False)
 def hello_bnb():
     return "Hello HBNB!"
